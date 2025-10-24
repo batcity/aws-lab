ebfeea0e7653d8e7daffe53981e55ecea9c9ed3a
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-10-24 23:16:04 +0530

    get started with API gateways in AWS

--- apigateway/apigateway.py
+++ apigateway/apigateway.py
@@ -3,3 +3,15 @@ import boto3
 apigateway = boto3.client("apigateway", endpoint_url="http://localhost:4566")
 rest_api = apigateway.create_rest_api(name="MyAPI")
 print("API created:", rest_api["id"])
+print("API retrieval example:")
+print(apigateway.get_rest_api(restApiId=rest_api["id"]))
+
+
+print("Listing all APIs:")
+for api in apigateway.get_rest_apis()["items"]:
+    print(api["name"], api["id"])
+    
+print("deleting all APIs:")
+for api in apigateway.get_rest_apis()["items"]:
+    apigateway.delete_rest_api(restApiId=api["id"])
+print("All APIs deleted.")
